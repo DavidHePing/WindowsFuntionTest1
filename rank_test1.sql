b284090536d1dbcae78a3c6ceb4784b1525da8c1
Dav He <s991179@gmail.com>
2025-03-27 12:14:16 +0800

    feat: rank

---
+++ rank_test1.sql
@@ -0,0 +1,15 @@
+-- select everyone's object order by grade, the same data has same order
+-- and next level would + 1
+SELECT 
+    name, 
+    object, 
+    grade,
+    RANK() OVER (
+        PARTITION BY name 
+        ORDER BY grade DESC
+    ) AS grade_rank
+FROM student_score
+
+-- "Alice"	"Math"	    88	1
+-- "Alice"	"English"	88	1
+-- "Alice"	"Physics"	72	3
